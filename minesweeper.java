d72c8a3323d64c5b87e4fc3f858da35ab9011e48
JuanDGiraldoM <juandagiraldoma@gmail.com>
2018-09-05 20:31:33 -0500

    Init the minesweeper

--- minesweeper.java
+++ minesweeper.java
@@ -8,6 +8,7 @@ public class minesweeper {
     Scanner scanner = new Scanner(System.in);
     char[] input;
     char[][][] minesweeper;
+    int coveredCells;
 
     //instructions
     System.out.println("\n--------------------Minesweeper Game!!--------------------\n");
@@ -22,4 +23,45 @@ public class minesweeper {
     System.out.println("--Represent a flag -> 'p'");
     System.out.println("--Number of adjacent mines -> '1...8'");
     System.out.println();
+
+    //initialize the minesweeper
+    System.out.print("How do you want your minesweeper?: ");
+    input = scanner.nextLine().split(" ");
+    int rows = Integer.parseInt(input[0]);
+    int columns = Integer.parseInt(input[1]);
+    int mines = Integer.parseInt(input[2]);
+    minesweeper = InitializeMinesweeper(rows, columns, mines);
+    coveredCells = rows * columns;
+
+    do {
+
+
+    }
+    while (coveredCells);
+
+  }
+
+  static char[][][] InitializeMinesweeper(int rows, int columns, int mines) {
+    char[][][] minesweeper = new char[rows][columns][2];
+
+    for (char[][] column : minesweeper) {
+      for (char[] cell : column) {
+        cell[0] = '.';
+        cell[1] = '';
+      }
+    }
+
+    int row = 0, column = 0;
+    while (mines) {
+      row = (int) (Math.random() * rows);
+      column = (int) (Math.random() * columns);
+
+      if (minesweeper[row][column][1] != '*'){
+        minesweeper[row][column][1] = '*';
+        --mines;
+      }
+    }
+
+    return minesweeper;
+  }
 }
